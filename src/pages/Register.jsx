fabe9ffcc65b679d3e251710de0cb90d007af757
Gautam Kumar <129857715+whoisgautxm@users.noreply.github.com>
2024-02-03 18:51:54 +0530

    Database Connected and Routing Done
    Routing from starting page -> instructions page -> questions page is done.
    and user context is created to extract user data while submitting.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import Spline from "@splinetool/react-spline";
+import { useState } from "react";
+import supabase from "../config/supabseClient";
+import { useUser } from "../context/UserContext";
 function Register() {
   const navigate = useNavigate();
+  
   const handleLogin = (path) => {
     navigate(path);
   };
   const gradientStyle = {
-    background: 'linear-gradient(180deg, #F03 0%, #FA9596 100%)',
-    height: '100vh', // Adjust the height as needed
+    background: "linear-gradient(180deg, #F03 0%, #FA9596 100%)",
+    height: "100vh", // Adjust the height as needed
   };
   const backgroundcolor = {
     backgroundColor: "#F5F5F5",
@@ -19,6 +23,35 @@ function Register() {
   };
   const textcolor = {
     color: "#FF0033",
+  };
+  const { updateUser } = useUser();
+  const [name, setName] = useState("");
+  const [className, setClassName] = useState("");
+  const [age, setAge] = useState("");
+  const [formError, setFormError] = useState(null);
+  const handleSubmission =  (e) => {
+    
+    e.preventDefault();
+   
+    if (!name || !className || !age) {
+      setFormError("Please fill out all the fields");
+      return;
+    }
+    const { data, error } =  supabase
+      .from("Register")
+      .insert([{ name, className, age }]);
+    if (error) {
+      console.log(error);
+      setFormError("An error occured while submitting the form");
+      return;
+    }
+    if (data) {
+      console.log(data);
+      setFormError(null);
+      // updateUser(name);
+      navigate("/instructions");
+    }
+    
   };
 
   return (
@@ -34,38 +67,42 @@ function Register() {
           <h2>Name:</h2>
           <input
             type="text"
-            placeholder="Place Your Name"
+            placeholder="Place Enter Your Name"
             className="bg-custom-grey rounded-md p-2 text-center"
             style={backgroundcolor}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <h2>Class:</h2>
           <input
             type="text"
-            placeholder="Place Your Name"
+            placeholder="Place Enter Your Class"
             className="bg-custom-grey rounded-md p-2 text-center"
             style={backgroundcolor}
+            value={className}
+            onChange={(e) => setClassName(e.target.value)}
           />
           <h2>Age:</h2>
           <input
             type="text"
-            placeholder="Place Your Name"
+            placeholder="Place Enter Your Age"
             className="bg-custom-grey rounded-md p-2 text-center"
             style={backgroundcolor}
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
           />
           <button
             className="bg-custom-red py-2 rounded-md self-center w-full text-white"
             style={backgroundcolorbutton}
+            onClick={handleSubmission}
           >
             <div className="flex flex-row justify-center gap-2">
               <p>Let's Get Started!</p>
               <FaArrowRightLong className="text-2xl" />
             </div>
           </button>
-          <div className="flex flex-row">
-            <hr className="w-1/2 self-center"></hr>
-            <p className="text-xs">or</p>
-            <hr className="w-1/2 self-center"></hr>
-          </div>
+          {formError && <p className="text-red-700">{formError}</p>}
+
           <div className="flex flex-row gap-1">
             <p>Already have an account?</p>
             <a
